a564cab114dda83209b563a94105f75796cba98d
Tim Asp <timothy.asp@gmail.com>
2018-10-30 22:09:24 -0700

    Add indicator to language dialog to show current language

--- App/src/com/dozuki/ifixit/ui/LanguageDialogFragment.java
+++ App/src/com/dozuki/ifixit/ui/LanguageDialogFragment.java
@@ -3,10 +3,12 @@ package com.dozuki.ifixit.ui;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 
 import com.dozuki.ifixit.R;
+import com.dozuki.ifixit.util.LocaleManager;
 
 public class LanguageDialogFragment extends DialogFragment {
    public static LanguageDialogFragment newInstance() {
@@ -16,11 +18,21 @@ public class LanguageDialogFragment extends DialogFragment {
 
    @Override
    public Dialog onCreateDialog(Bundle savedInstanceState) {
+      Resources res = getResources();
+      final String[] codes = res.getStringArray(R.array.languagesCodes);
+      String[] languages = res.getStringArray(R.array.languages);
+
+      for (int i = 0; i < codes.length; i++) {
+         if (LocaleManager.getLanguage(getActivity()).equalsIgnoreCase(codes[i])) {
+            languages[i] += " " + getResources().getString(R.string.current);
+         }
+      }
+
       AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
       builder.setTitle(R.string.select_language)
-       .setItems(R.array.languages, new DialogInterface.OnClickListener() {
+       .setItems(languages, new DialogInterface.OnClickListener() {
           public void onClick(DialogInterface dialog, int which) {
-             String languageCode = getResources().getStringArray(R.array.languagesCodes)[which];
+             String languageCode = codes[which];
              ((OnLanguageSelectListener)getActivity()).onLanguageSelect(languageCode);
           }
        });
